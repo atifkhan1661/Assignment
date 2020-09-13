219af367e1163b968a31e78b232387080771f173
atifkhan1661 <atifkhan1661@gmail.com>
2020-09-13 07:03:40 -0700

    first commit

---
+++ AtifKhan/Controllers/schoolsController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AtifKhan.Models;
+
+namespace AtifKhan.Controllers
+{
+    public class schoolsController : Controller
+    {
+        private schoolDBContext db = new schoolDBContext();
+
+        // GET: schools
+        public ActionResult Index(string searchString)
+        {
+            
+
+            var movies = from m in db.Movies
+                         select m;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                movies = movies.Where(s => s.Title.Contains(searchString));
+            }
+            return View(movies);
+        }
+
+        // GET: schools/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            school school = db.Movies.Find(id);
+            if (school == null)
+            {
+                return HttpNotFound();
+            }
+            return View(school);
+        }
+
+        // GET: schools/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: schools/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Title,ReleaseDate,Genre,Price")] school school)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Movies.Add(school);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(school);
+        }
+
+        // GET: schools/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            school school = db.Movies.Find(id);
+            if (school == null)
+            {
+                return HttpNotFound();
+            }
+            return View(school);
+        }
+
+        // POST: schools/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Title,ReleaseDate,Genre,Price")] school school)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(school).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(school);
+        }
+
+        // GET: schools/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            school school = db.Movies.Find(id);
+            if (school == null)
+            {
+                return HttpNotFound();
+            }
+            return View(school);
+        }
+
+        // POST: schools/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            school school = db.Movies.Find(id);
+            db.Movies.Remove(school);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
